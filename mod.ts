3bd42a121c53cce3c58913ecf7efef04cc426a6f
Gnlow <entrydark@gmail.com>
2020-06-23 12:28:04 +0900

    Revert "make properties non-enumerable"
    This reverts commit 58f4bb7885471da4d68d26bd857a1458dbf40274.

--- mod.ts
+++ mod.ts
@@ -1,21 +1,15 @@
-const infiniteArray = <T>(main: (index: number) => T) => {
-    const source = {}
-    Object.defineProperties(source, {
-        [Symbol.iterator]: {
-            enumerable: false,
-            value: function*(){
-                let index = 0
-                while(true) {
-                    yield main(index++)
-                }
+const infiniteArray = <T>(main: (index: number) => T) => 
+    new Proxy({
+        *[Symbol.iterator](){
+            let index = 0
+            while(true) {
+                yield main(index++)
             }
         },
-        [Symbol.toStringTag]: {
-            enumberable: false,
-            get: () => `InfiniteArray (${main(0)}, ${main(1)}, ...)`
+        get [Symbol.toStringTag]() {
+            return `InfiniteArray (${main(0)}, ${main(1)}, ...)`
         }
-    })
-    return new Proxy(source as {
+    } as {
         [index: number]: T
         [Symbol.iterator]: GeneratorFunction
         [Symbol.toStringTag]: any
@@ -33,7 +27,7 @@ const infiniteArray = <T>(main: (index: number) => T) => {
             }
         }
     })
-}
+
 const plant = infiniteArray(x => x*2)
 
 console.log(plant)
