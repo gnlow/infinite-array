388a0e0607cdbcd9f37daa83cf4ebf64079f4d4c
Gnlow <entrydark@gmail.com>
2020-06-23 12:14:16 +0900

    Add Symbol.toStringTag

--- mod.ts
+++ mod.ts
@@ -4,10 +4,14 @@ const infiniteArray = <T>(main: (index: number) => T) => new Proxy({
         while(true) {
             yield main(index++)
         }
+    },
+    get [Symbol.toStringTag]() {
+        return `InfiniteArray (${main(0)}, ${main(1)}, ...)`
     }
 } as {
     [index: number]: T
     [Symbol.iterator]: GeneratorFunction
+    [Symbol.toStringTag]: any
 }, {
     get: (target, index: any) => {
         if(index in target) {
