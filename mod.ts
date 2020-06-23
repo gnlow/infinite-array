c619e51e3c781fed537499ee52f101ba4c33152e
Gnlow <entrydark@gmail.com>
2020-06-23 12:15:19 +0900

    fix indent

--- mod.ts
+++ mod.ts
@@ -1,31 +1,32 @@
-const infiniteArray = <T>(main: (index: number) => T) => new Proxy({
-    *[Symbol.iterator](){
-        let index = 0
-        while(true) {
-            yield main(index++)
+const infiniteArray = <T>(main: (index: number) => T) => 
+    new Proxy({
+        *[Symbol.iterator](){
+            let index = 0
+            while(true) {
+                yield main(index++)
+            }
+        },
+        get [Symbol.toStringTag]() {
+            return `InfiniteArray (${main(0)}, ${main(1)}, ...)`
         }
-    },
-    get [Symbol.toStringTag]() {
-        return `InfiniteArray (${main(0)}, ${main(1)}, ...)`
-    }
-} as {
-    [index: number]: T
-    [Symbol.iterator]: GeneratorFunction
-    [Symbol.toStringTag]: any
-}, {
-    get: (target, index: any) => {
-        if(index in target) {
-            return target[index]
+    } as {
+        [index: number]: T
+        [Symbol.iterator]: GeneratorFunction
+        [Symbol.toStringTag]: any
+    }, {
+        get: (target, index: any) => {
+            if(index in target) {
+                return target[index]
+            }
+            if(
+                typeof index == "number" 
+                || typeof index == "string" 
+                && !Number.isNaN(Number(index))
+            ) {
+                return main(Number(index))
+            }
         }
-        if(
-            typeof index == "number" 
-            || typeof index == "string" 
-            && !Number.isNaN(Number(index))
-        ) {
-            return main(Number(index))
-        }
-    }
-})
+    })
 
 const plant = infiniteArray(x => x*2)
 
