95700bfba62f376e1bc18d3a78a5dcf477a9e8bc
Gnlow <entrydark@gmail.com>
2020-10-27 23:45:24 +0900

    type extends from `Iterable`

--- mod.ts
+++ mod.ts
@@ -1,29 +1,36 @@
-export default <T>(main: (index: number) => T) => 
-    new Proxy({
-        *[Symbol.iterator](){
-            let index = 0
-            while(true) {
-                yield main(index++)
+interface InfiniteArray<T> extends Iterable<T> {
+    [index: number]: T
+    [Symbol.toStringTag]: string
+}
+
+export default <T>(main: (index: number) => T): InfiniteArray<T> => 
+    new Proxy(
+        {
+            *[Symbol.iterator](){
+                let index = 0
+                while(true) {
+                    yield main(index++)
+                }
+            },
+            get [Symbol.toStringTag]() {
+                return `InfiniteArray (${main(0)}, ${main(1)}, ...)`
             }
-        },
-        get [Symbol.toStringTag]() {
-            return `InfiniteArray (${main(0)}, ${main(1)}, ...)`
+        } as {
+            [index: number]: T
+            [Symbol.iterator]: () => Iterator<T>
+            [Symbol.toStringTag]: string
+        }, {
+            get: (target, index: any) => {
+                if(index in target) {
+                    return target[index]
+                }
+                if(
+                    typeof index == "number" 
+                    || typeof index == "string" 
+                    && !Number.isNaN(Number(index))
+                ) {
+                    return main(Number(index))
+                }
+            }
         }
-    } as {
-        [index: number]: T
-        [Symbol.iterator]: GeneratorFunction
-        [Symbol.toStringTag]: any
-    }, {
-        get: (target, index: any) => {
-            if(index in target) {
-                return target[index]
-            }
-            if(
-                typeof index == "number" 
-                || typeof index == "string" 
-                && !Number.isNaN(Number(index))
-            ) {
-                return main(Number(index))
-            }
-        }
-    })
+    )
